8a118581e28515fed9346611b8062ecf85bf4bd2
ChikenUni <hlindgren.hemma@gmail.com>
2024-09-24 15:35:21 +0200

    Tests for the Kademlia and Kademliaid files

---
+++ kademlia/kademlia_test.go
@@ -0,0 +1,81 @@
+package kademlia
+
+import (
+	"testing"
+	"time"
+)
+
+func TestProcessContactLookupReturns(t *testing.T) {
+	kadTest := Init()
+	dataChan := kadTest.network.findContactCh
+	go dataspoofer(dataChan)
+	targetID := NewKademliaID("FFFFFFFF0000000000000000000000000000000F")
+	targetAdr := "172.0.0.10"
+	targetContact := NewContact(targetID, targetAdr)
+	returnedList := kadTest.ProcessContactLookupReturns(targetContact.ID)
+
+	// These are the contacts we'll be placing in the reference list
+	id1 := NewKademliaID("FFFFFFFF00000000000000000000FF0000000000")
+	adr1 := "172.0.0.4"
+	cont1 := NewContact(id1, adr1)
+	id2 := NewKademliaID("FFFFFFFF0000000000000000000000FF00000000")
+	adr2 := "172.0.0.5"
+	cont2 := NewContact(id2, adr2)
+	id3 := NewKademliaID("FFFFFFFF000000000000000000000000FF000000")
+	adr3 := "172.0.0.6"
+	cont3 := NewContact(id3, adr3)
+	id4 := NewKademliaID("FFFFFFFF00000000000000000000000000FF0000")
+	adr4 := "172.0.0.7"
+	cont4 := NewContact(id4, adr4)
+	id5 := NewKademliaID("FFFFFFFF0000000000000000000000000000FF00")
+	adr5 := "172.0.0.8"
+	cont5 := NewContact(id5, adr5)
+	id6 := NewKademliaID("FFFFFFFF000000000000000000000000000000FF")
+	adr6 := "172.0.0.9"
+	cont6 := NewContact(id6, adr6)
+
+	var referenceList []Contact
+	referenceList = append(referenceList, cont6, cont5, cont4, cont3, cont2, cont1)
+
+	for i := 0; i < len(returnedList); i++ {
+		if returnedList[i].ID.String() != referenceList[i].ID.String() || returnedList[i].Address != referenceList[i].Address {
+			// If address or ID doesn't match the reference list something is wrong
+			t.Errorf("TestProcessContactLookupReturns failed, got %v, %v, expected %v, %v", returnedList[i].ID, returnedList[i].Address, referenceList[i].ID, referenceList[i].Address)
+		}
+	}
+}
+
+func dataspoofer(datachannel chan Contact) {
+	id1 := NewKademliaID("FFFFFFFF00000000000000000000FF0000000000")
+	adr1 := "172.0.0.4"
+	cont1 := Contact{id1, adr1, id1}
+	id2 := NewKademliaID("FFFFFFFF0000000000000000000000FF00000000")
+	adr2 := "172.0.0.5"
+	cont2 := Contact{id2, adr2, id2}
+	id3 := NewKademliaID("FFFFFFFF000000000000000000000000FF000000")
+	adr3 := "172.0.0.6"
+	cont3 := Contact{id3, adr3, id3}
+	datachannel <- cont1
+	datachannel <- cont2
+	datachannel <- cont3
+	//^ Initial Contacts provided to the method
+	time.Sleep(4 * time.Second)
+	id4 := NewKademliaID("FFFFFFFF00000000000000000000000000FF0000")
+	adr4 := "172.0.0.7"
+	cont4 := Contact{id4, adr4, id4}
+	id5 := NewKademliaID("FFFFFFFF0000000000000000000000000000FF00")
+	adr5 := "172.0.0.8"
+	cont5 := Contact{id5, adr5, id5}
+	id6 := NewKademliaID("FFFFFFFF000000000000000000000000000000FF")
+	adr6 := "172.0.0.9"
+	cont6 := Contact{id6, adr6, id6}
+	datachannel <- cont4
+	datachannel <- cont5
+	datachannel <- cont6
+	//^ Second round of contacts provided to the method
+	time.Sleep(2 * time.Second)
+	datachannel <- cont4
+	datachannel <- cont5
+	datachannel <- cont6
+	// resending the same messages again
+}
--- kademlia/kademliaid_test.go
+++ kademlia/kademliaid_test.go
@@ -37,3 +37,27 @@ func TestEquals(t *testing.T) {
 		t.Errorf("TestEquals failed, got %v, %v - expected True, False", equalTrue, equalFalse)
 	}
 }
+
+func TestDistance(t *testing.T) {
+	testID1 := NewKademliaID("FFFFFFFF0000000000000000000000000000000F")
+	testID2 := NewKademliaID("FFFFFFFF000000000000000000000000000000FF")
+	testID3 := NewKademliaID("FFFFFFFF000000000000000000000000000000F0")
+
+	dist1 := testID1.CalcDistance(*testID1) // This should be zero
+	dist2 := testID1.CalcDistance(*testID2) // This should be 0x000...0f0
+	dist3 := testID1.CalcDistance(*testID3) // This should be 0x000...0ff
+
+	correctDist1 := NewKademliaID("0000000000000000000000000000000000000000")
+	correctDist2 := NewKademliaID("00000000000000000000000000000000000000f0")
+	correctDist3 := NewKademliaID("00000000000000000000000000000000000000ff")
+
+	if dist1.String() != correctDist1.String() {
+		t.Errorf("TestEquals failed, got %v, expected %v", dist1, correctDist1)
+	}
+	if dist2.String() != correctDist2.String() {
+		t.Errorf("TestEquals failed, got %v, expected %v", dist2, correctDist2)
+	}
+	if dist3.String() != correctDist3.String() {
+		t.Errorf("TestEquals failed, got %v, expected %v", dist3, correctDist3)
+	}
+}
